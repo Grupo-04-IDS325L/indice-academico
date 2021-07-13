2e3b44a1eacaf562e71209fe4fa65460f2098948
Waldo Ruiz <wruiz@wind.com.do>
2021-07-12 20:06:40 -0400

    'Consults that student with Id 10800241 doesn't exists' test added; 'Consults that student with Id 1080024 has testNoBorrar by Name' added

--- cy_tests/cypress/integration/student.spec.js
+++ cy_tests/cypress/integration/student.spec.js
@@ -1,6 +1,11 @@
 describe("Student`s section test", () => {
-    it("Consults a that testNoBorrar is the name of student with Id 1080024", () => {
-      cy.visit("http://localhost:3000/students/1080024");
-      cy.get(".left-side > :nth-child(4)").should("have.text", "testNoBorrar");
-    })
+    it("Consults that student with Id 10800241 doesn't exists", () => {
+      cy.visit("http://localhost:3000/students/10800241");
+      cy.url().should("not.include", "students/10800241");
+    }),
+    it("Consults that student with Id 1080024 has testNoBorrar by Name", () => {
+        cy.visit("http://localhost:3000/students/1080024");
+        cy.url().should("include", "students/1080024");
+        cy.get(".left-side > :nth-child(4)").should("have.text", "testNoBorrar");
+      })
   })
