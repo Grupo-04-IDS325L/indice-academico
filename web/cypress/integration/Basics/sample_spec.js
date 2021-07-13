4cd92e4fdc5a1c5f95aa9e81ac5c52b05f49390f
Alam Sierra <alamsierra@10.0.0.8>
2021-07-13 09:33:28 -0400

    restructure directory

--- web/cypress/integration/Basics/sample_spec.js
+++
@@ -1,5 +0,0 @@
-describe("First test case", function() {
-    it("Doesn't do much", function(){    
-        expect(true).to.equal(true)
-    })
-})
--- web/src/test/app.test.js
+++
@@ -1,6 +0,0 @@
-// El path relativo de lo que quieren probar va en require:
-//const comp = require("../components/Student/GradesTable/index");
-
-test("test jest", () => {
-	expect(true).toBe(true);
-});
