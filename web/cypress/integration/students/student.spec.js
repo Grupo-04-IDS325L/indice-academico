23dcc38ab3dad6e4612723afb510ef8fb01e518e
Alam <alam.15@outlook.com>
2021-07-13 09:59:52 -0400

    format code with eslint

--- web/cypress/integration/students/student.spec.js
+++ web/cypress/integration/students/student.spec.js
@@ -1,11 +1,11 @@
 describe("Student`s section test", () => {
-    it("Consults that student with Id 10800241 doesn't exists", () => {
-      cy.visit("http://localhost:3000/students/10800241");
-      cy.url().should("not.include", "students/10800241");
-    }),
+  it("Consults that student with Id 10800241 doesn't exists", () => {
+    cy.visit("http://localhost:3000/students/10800241");
+    cy.url().should("not.include", "students/10800241");
+  }),
     it("Consults that student with Id 1080024 has testNoBorrar by Name", () => {
-        cy.visit("http://localhost:3000/students/1080024");
-        cy.url().should("include", "students/1080024");
-        cy.get(".left-side > :nth-child(4)").should("have.text", "testNoBorrar");
-      })
-  })
+      cy.visit("http://localhost:3000/students/1080024");
+      cy.url().should("include", "students/1080024");
+      cy.get(".left-side > :nth-child(4)").should("have.text", "testNoBorrar");
+    });
+});
--- web/cypress/integration/teachers/teacher.spec.js
+++ web/cypress/integration/teachers/teacher.spec.js
@@ -1,19 +1,18 @@
-describe("test teacher registration process", function() {
-  it("visits teacher dashboard", function() {
+describe("test teacher registration process", function () {
+  it("visits teacher dashboard", function () {
     cy.visit("http://localhost:3000/teachers");
     cy.url().should("include", "teachers");
   });
 
   it("writes the teacher's name in the registration form", () => {
-		cy.get("input[type='text']").type("John McCoffee");
-	});
+    cy.get("input[type='text']").type("John McCoffee");
+  });
 
-	it("submits the teacher form for registration", () => {
-		cy.get("button").click();
-    cy.wait(10000);
-	});
+  it("submits the teacher form for registration", () => {
+    cy.get("button").click();
+  });
 
   it("checks if the teacher has been created", () => {
-    cy.get("tbody:last-child td").contains("John McCoffee", {timeout: 60000});
+    cy.get("tbody:last-child td").contains("John McCoffee", { timeout: 60000 });
   });
-})
+});
