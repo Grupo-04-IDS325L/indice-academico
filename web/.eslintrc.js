43e26e560110c3b61a75faed611cfbe011d98fe0
Alam <alam.15@outlook.com>
2021-07-15 18:31:19 -0400

    resolve eslint bug

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:cypress/recommended",
     "react-app",
     "react-app/jest",
